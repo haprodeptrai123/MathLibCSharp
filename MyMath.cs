36c3d9673d89d8305bc173e9026c1a3cf606175d
haprodeptrai123 <68005492+haprodeptrai123@users.noreply.github.com>
2021-06-25 14:16:33 +0700

    Add files via upload

---
+++ MyMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public class MyMath
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public int Multi(int x, int y)
+        {
+            return x * y;
+        }
+        public int Div(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
